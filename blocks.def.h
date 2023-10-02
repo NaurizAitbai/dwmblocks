d23c28501ec2e14d4b2c2a5004615d26ee658548
Nauriz Aitbai <nauriz.aitbai@gmail.com>
2023-10-02 10:50:08 +0500

    added REC in bar;

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/                        /*Update Interval*/     /*Update Signal*/
-	{"MEM:",        "memory",                          30,                     0},
-        {"VOL:",        "volume",                           0,                    10},
-	{"",            "date '+%b %d (%a) %H:%M:%S'",      1,                     0},
+	/*Icon*/	    /*Command*/                                     /*Update Interval*/     /*Update Signal*/
+    {"REC:",        "cat /tmp/recordingstatus 2>/dev/null",          0,                     9},
+    {"MEM:",        "memory",                                       30,                     0},
+    {"VOL:",        "volume",                                        0,                    10},
+    {"",            "date '+%b %d (%a) %H:%M:%S'",                   1,                     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
